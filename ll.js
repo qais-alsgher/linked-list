5f8df3ade216e733afab2c7b72e67e7b68f3ebf6
qais-alsgher <qaisalsgher@gmail.com>
2022-09-10 02:55:53 +0300

    creat dleat method and test it

--- ll.js
+++ ll.js
@@ -25,10 +25,59 @@ class LinkedList {
         } else {
             this.tail.next = node;
             this.tail = node;
-
         }
     }
 
-}
+    numberNode() {
+        let num = 0;
+        let curentNode = this.head;
+        while (curentNode.next) {
+            curentNode = curentNode.next;
+            num++;
+        }
+        return num;
+    }
 
+    deleteMidle() {
+        if (!this.head) {
+            console.log('the list is empty')
+        } else if (!this.head.next || !this.head.next.next) {
+            this.head = null;
+            this.tail = null
+        } else {
+            const num = this.numberNode();
+            let previousND = this.head;
+            for (let i = 0; i < (Math.ceil(num / 2) - 2); i++) {
+                previousND = previousND.next;
+            }
+            if (num % 2 === 0) {
+                const deleteNode = previousND.next;
+                previousND.next = deleteNode.next;
+                deleteNode.next = null;
+            } else {
+                const deleteNode = previousND.next.next;
+                previousND.next = deleteNode.next;
+                deleteNode.next = null;
+            }
+        }
+    }
+
+
+
+
+    // insertInTo(value) {
+    //     let curentNode = this.head;
+    //     while ()
+    // }
+
+
+
+    displiy() {
+        curentNode = this.head;
+        while (curentNode.next) {
+            console.log(curentNode);
+            curentNode.next;
+        }
+    }
+}
 module.exports = LinkedList;

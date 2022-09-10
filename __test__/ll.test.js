57002cd61d01d9745f42aa138bf8d6e489b38c32
qais-alsgher <qaisalsgher@gmail.com>
2022-09-10 18:00:18 +0300

    insert in spicific location

--- __test__/ll.test.js
+++ __test__/ll.test.js
@@ -26,7 +26,18 @@ describe('test ll insert ', () => {
         expect(ll.tail.next).toBeNull();
         expect(ll.head.value).toEqual(3);
         expect(ll.head.next.value).toEqual(2);
-    })
+    });
+
+
+    it('test insert last when empty ll', () => {
+        const ll = new LinkedList();
+        ll.append(55);
+        expect(ll).toBeInstanceOf(LinkedList);
+        expect(ll.tail.value).toEqual(55);
+        expect(ll.tail.next).toBeNull();
+        expect(ll.head.value).toEqual(55);
+    });
+
 })
 
 
@@ -74,9 +85,50 @@ describe('test ll delete', () => {
         ll.insert(14);
         ll.insert(15);
         ll.deleteMidle();
-        const size = ll.numberNode();
         expect(ll).toBeInstanceOf(LinkedList);
         expect(ll.head.next.next.value).toEqual(8);
         expect(ll.tail.next).toBeNull();
     })
 })
+
+// index of element is start from 0
+describe('test ll for insert', () => {
+    it('test ll for insert in to first ll ', () => {
+        const ll = new LinkedList();
+        ll.insert(1);
+        ll.insert(13);
+        ll.insert(14);
+        ll.insertInTo(20, 0);
+        expect(ll).toBeInstanceOf(LinkedList);
+        expect(ll.head.value).toEqual(20);
+        expect(ll.head.next.value).toEqual(14);
+    });
+
+    it('test ll for insert in to specific location', () => {
+        const ll = new LinkedList();
+        ll.insert(5);
+        ll.insert(8);
+        ll.insert(1);
+        ll.insert(13);
+        ll.insert(14);
+        ll.insertInTo(20, 1);
+        expect(ll).toBeInstanceOf(LinkedList);
+        expect(ll.head.next.value).toEqual(20);
+        expect(ll.head.next.next.value).toEqual(13)
+    });
+
+    it('test ll for insert in to last ll', () => {
+        const ll = new LinkedList();
+        ll.insert(1);
+        ll.insert(13);
+        ll.insert(14);
+        ll.insertInTo(20, 3);
+        ll.insertInTo(55, 4);
+        expect(ll).toBeInstanceOf(LinkedList);
+        expect(ll.head.next.next.next.value).toEqual(20);
+        // expect(ll.head.next.next.next.next.value).toEqual(55);
+        expect(ll.tail.value).toEqual(55);
+        expect(ll.head.next.next.next.next.value).toEqual(55);
+    });
+
+});
